08396924393931109f7b7401b40e39c65d1f563b
Henzie <henzie0203@gmail.com>
2026-06-30 21:25:34 +0200

    feat: add utils for dependecies and authentication

--- Backend/utils/dependencies.py
+++ Backend/utils/dependencies.py
@@ -2,7 +2,7 @@ from typing import Generator
 from ..database.database import SessionlLocal 
 
 """
-create a new database session  for a single network request
+create a new database session for a single network request
 and auto close it once the request is done
 """
 def get_db() -> Generator:
