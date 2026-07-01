8e201dcfc23f83a0c281611a9269229fbfa05f3f
Henzie <henzie0203@gmail.com>
2026-07-02 00:08:30 +0200

    update user models

--- Backend/app/models/user.py
+++ Backend/app/models/user.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Column, String, Integer, DateTime, Boolean
-from database import Base
-from datetime import datetime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from datetime import datetime
+from ..database import Base
+
 
 class UserModel(Base):
     __tablename__ = "users"
@@ -10,7 +12,10 @@ class UserModel(Base):
     username = Column(String, nullable=False, unique=True, index=True)
     email = Column(String, nullable=False, unique=True, index=True)
     passowrd = Column(String, nullable=False)
+    bio = Column(String, default="")
+    profile_pic = Column(String, default="")
     is_active = Column(Boolean, default=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     posts = relationship("PostModel", back_populates="author", cascade="all, delete-orphan")
