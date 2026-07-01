547d9606c6366dc7509ee49cb734b377eda17ab5
Henzie <henzie0203@gmail.com>
2026-07-01 23:30:29 +0200

    setup configuration

---
+++ Backend/app/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from typing import List
+
+class configSettings(BaseSettings):
+    DATABASE_URL: str
+    SECRETE_KEY:str
+    ALGORITHM: str
+    TOKEN_EXPIRE_MINUTES: int
+    CORS_ORIGIN: List[str]
+    
+    class config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+setting = Settings()
